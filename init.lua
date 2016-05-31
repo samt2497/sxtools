c10d08aef02449412906f9c41b788a5308f76051
Simon Mora <smora@rootstack.com>
2016-05-31 16:56:57 -0500

    Add zipcodes.

--- init.lua
+++ init.lua
@@ -1,8 +1,46 @@
-_G.test = function() 
-	print('test');
+local exports = {}
+local lib = {}
+local stats = {
+  functions = 0
+}
+local storage = {
+
+}
+-- Libraries.
+lib.json = require('json')
+lib.fs = require('fs')
+lib.path = require('path')
+
+-- Functions.
+local readFile = function(path)
+  local f = io.open(path, "rb")
+  local content = f:read("*all")
+  f:close()
+  return content
 end
-_G.die = function() 
+local init = function()
+  local data = readFile('/home/samt2497/sxtools/data/zips.json')
+  local codes = lib.json.parse(data)
+  storage.zipCodes = codes
+end
+exports.die = function()
 	os.exit(0)
 end
-_G['nope'] = 'lol'
-print('SXI Loaded')
+
+exports.reload = function()
+
+end
+exports.random = {}
+exports.random.zip = function()
+  return storage.zipCodes[math.random(1,#storage.zipCodes)]
+end
+
+for key, pointer in pairs(exports) do
+  _G[key] = pointer
+  stats.functions = stats.functions + 1
+end
+init()
+_G['sxi_status'] = {
+  loaded = true
+}
+print('SXI Loaded: ' .. stats.functions  .. ' funcs')
